31f967e243d9455d046ec0f608f0bad0fa578a38
elicul <enzo.licul@yahoo.com>
2019-10-14 19:57:34 +0200

    POC implemented

--- SignalR/SignalRClient/src/app/app.component.ts
+++ SignalR/SignalRClient/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as signalR from '@aspnet/signalr';
+import { MessageService } from 'primeng/components/common/messageservice';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService]
 })
-export class AppComponent {
-  title = 'SignalRClient';
+export class AppComponent implements OnInit {
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    const connection = new signalR.HubConnectionBuilder()
+      .configureLogging(signalR.LogLevel.Information)
+      .withUrl('https://localhost:5628/notify')
+      .build();
+
+    connection
+      .start()
+      .then(() => {
+        console.log('Connected!');
+      })
+      .catch(err => {
+        return console.error(err.toString());
+      });
+
+    connection.on('BroadcastMessage', (type: string, payload: string) => {
+      this.messageService.add({
+        severity: type,
+        summary: payload,
+        detail: 'Via SignalR'
+      });
+    });
+  }
 }
--- SignalR/SignalRClient/src/app/app.module.ts
+++ SignalR/SignalRClient/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ToastModule } from 'primeng/toast';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
